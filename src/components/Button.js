d1a962f6d89e50a36c7b4f0635a7d76056f0146d
Nadia <kasimnadia@gmail.com>
2022-01-23 22:54:11 -0500

    imported and implemented classnames to compose list of classes for button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,18 +1,24 @@
 import React from "react";
-
 import "components/Button.scss";
+import classNames from "classnames";
+
 
 export default function Button(props) {
    // this is the base class 
-   let buttonClass = "button";
+   // let buttonClass = "button";
+   // if (props.confirm) {
+   //    buttonClass += " button--confirm";
+   // }
+   // if (props.danger) {
+   //    buttonClass += " button--danger";
+   // }
 
-   if (props.confirm) {
-      buttonClass += " button--confirm";
-   }
-
-   if (props.danger) {
-      buttonClass += " button--danger";
-   }
+   // refactor above if-statements to use classNames to clean up code 
+      // if props.confirm is true than we want to access button--confirm
+   const buttonClass = classNames("button", 
+   {"button--confirm": props.confirm}, 
+   {"button--danger":props.danger}
+   );
 
    return (
       <button className={buttonClass} 
