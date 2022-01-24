86d5055df49581d67090a08abc2e7bbe19844586
Nadia <kasimnadia@gmail.com>
2022-01-24 03:35:46 -0500

    created InterviewerListItem component, included conditionals and event handlers, ran tests + styling

--- src/components/Application.js
+++ src/components/Application.js
@@ -55,3 +55,4 @@ export default function Application(props) {
     </main>
   );
 }
+
---
+++ src/components/InterviewerList.js
@@ -0,0 +1,18 @@
+// holds all of the InterviewerListItem components together
+
+
+
+
+import React from 'react';
+
+const InterviewerList = function (props) {
+
+  return (
+
+    <div></div>
+  )
+
+
+}
+
+export default InterviewerList;
