596789f2be4c5bf4309851e8ee276ce46e5e4fd1
Nadia <kasimnadia@gmail.com>
2022-01-25 03:03:08 -0500

    fixed bug so now lits of interviewers and their avis shows + cleaned up code a bit

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,24 +1,3 @@
-/* represents ONE interviewer and has two states: 
-1. when it is not selected it only has their image
-2. when it is selected, it will highlight item with a white background + show name of interviewer
-
-A single interview object looks as follows: 
-const interviewer = {
-  id: 1,
-  name: "Sylvia Palmer",
-  avatar: "https://i.imgur.com/LpaY82x.png"
-};
-
-Thus needs the following props: 
-  --- id:number ==> id of interviewer
-  --- name:string ==>  the name of the interviewer
-  --- avatar:url ==> a url to an image of the interviewer
-  --- selected:boolean - determines if an interviewer is selected or not and displays the name and applies appropriate styles if selected.
-  --- setInterviewer:function - is run when the InterviewerListItem is clicked. This function receives the interviewer's id as an argument. It sets the selected interviewer.
-
-
-*/
-
 import React from 'react';
 import 'components/InterviewerListItem.scss';
 import classNames from 'classnames';
