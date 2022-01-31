e7c29dac064879cd9bbcb05e13516aa049dd0e3e
Nadia <kasimnadia@gmail.com>
2022-01-31 18:49:38 -0500

    added test for navigating days

--- cypress/integration/navigaton.spec.js
+++ cypress/integration/navigaton.spec.js
@@ -1,5 +1,15 @@
 describe("Navigation", () => {
   it("should visit root", () => {
+  });
+
+  it("should navigate to Tuesday", () => {
     cy.visit("/");
+    // get the list item that contains text Tuesday and click on it
+    // cy.get("li")
+    // cy.contains("li", "Tuesday")
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      // .should("have.css", "background-color", "rgb(242, 242, 242)")
+      .should("have.class", "day-list__item--selected");
   });
 });
