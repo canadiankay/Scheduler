6862b6e7da12c4afd29e4ffc85830ee2d6e3c172
Nadia <kasimnadia@gmail.com>
2022-01-31 20:37:03 -0500

    add test for cancelling/deleting an  appt

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,12 +1,13 @@
 describe("Appointment", () => {
   beforeEach(() => {
     cy.request("GET", "/api/debug/reset")
-    cy.visit("/");
-  });
-  
-  it("should book an interview", () => {
-    cy.contains("Monday");
 
+    cy.visit("/");
+    
+    cy.contains("Monday");
+  });
+
+  it("should book an interview", () => {
     cy.get("[alt=Add]")
     // since there are two ADd buttons, we want to hide the second one
       .first()
@@ -19,8 +20,47 @@ describe("Appointment", () => {
       .click();
 
     cy.contains("Save").click();
+    
+    // verify that we show the student and interviewer names within and element
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Sylvia Palmer"); 
+  });
 
-        
+  it("should edit an interview", () => {
+    cy.get("[alt=Edit]")
+      .invoke("show")
+      .first()
+      .click({ force: true });
+
+    cy.get("[data-testid=student-name-input]")
+      .clear()
+      .type("Michie Lynn");
+
+    cy.get("[alt='Tori Malcolm']")
+      .click();
+
+    cy.contains("Save").click();
+
+    cy.contains(".appointment__card--show", "Michie Lynn");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
+  });
+
+  it("should cancel an interview", () => {
+    cy.get("[alt=Delete]")
+      .invoke("show")
+      .first()
+      .click({ force: true });
+
+    cy.contains("Confirm")
+      .click();
+
+    cy.contains("Deleting");
+    // after we press delete, we dont want there to be a deleting option anymore- we want it to disappear
+    cy.contains("Deleting")
+      .should("not.exist");
+
+    cy.contains(".appointment__card--show", "Archie Cohen")
+      .should("not.exist");
   });
 
 
